f6f15b6bf522ec979f2fdd146e6e92cf40069a84
Rupesh Kumar Hazari <rupeshhazari9168@gmail.com>
2022-02-27 00:16:03 +0530

    tutorials uploaded

---
+++ tut53.js
@@ -0,0 +1,29 @@
+console.log("This is tut 53");
+
+// function greet(name, greetText="Greetings from JavaScript"){
+//     console.log(greetText + " " + name)
+//     console.log(name + " is a good boy");
+// }
+
+// let name = "Hazari";
+// let name1 = "Kumar";
+// let name2 = "Rupesh"
+// let name3 = "Harry";
+// let greetText= "Good Morning";
+// greet(name, greetText)
+// greet(name1, greetText)
+// greet(name2, greetText)
+// greet(name3)
+// console.log(name + " is a good boy");
+// console.log(name1 + " is a good boy");
+// console.log(name2 + " is a good boy");
+// console.log(name3 + " is a good boy");
+
+function sum(a,b,c){
+    let d = a+b+c;
+    return d;
+    console.log("returned");
+}
+
+let result = sum(1,2,3);
+console.log(result);
